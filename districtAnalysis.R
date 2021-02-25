4b1d2f90502bc8f485b60f94924102c866bc597e
Mariah Bolden <ibotismariah@gmail.com>
2021-02-25 11:44:58 -0500

    Add the R project to the file

---
+++ districtAnalysis.R
@@ -0,0 +1,65 @@
+#This project uses the csv files I made from the blue alliance api to do further analysis
+
+#importing the csv files I will be working with
+
+#Michigan
+fim2015=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2015\\2015fim.csv",
+                   sep=",", header=TRUE)
+fim2016=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2016\\2016fim.csv",
+                   sep=",", header=TRUE)
+fim2017=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2017\\2017fim.csv",
+                   sep=",", header=TRUE)
+fim2018=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2018\\2018fim.csv",
+                   sep=",", header=TRUE)
+fim2019=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2019\\2019fim.csv",
+                   sep=",", header=TRUE)
+
+
+#Indiana 
+ind2015=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2015\\2015in.csv",
+                   sep=",", header=TRUE)
+ind2016=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2016\\2016in.csv",
+                   sep=",", header=TRUE)
+ind2017=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2017\\2017in.csv",
+                   sep=",", header=TRUE)
+ind2018=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2018\\2018in.csv",
+                   sep=",", header=TRUE)
+ind2019=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2019\\2019in.csv",
+                   sep=",", header=TRUE)
+
+#Mid Atlantic
+fma2015=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2015\\2015mar.csv",
+                   sep=",", header=TRUE)
+fma2016=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2016\\2016mar.csv",
+                   sep=",", header=TRUE)
+fma2017=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2017\\2017mar.csv",
+                   sep=",", header=TRUE)
+fma2018=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2018\\2018fma.csv",
+                   sep=",", header=TRUE)
+fma2019=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2019\\2019fma.csv",
+                   sep=",", header=TRUE)
+
+#New England
+ne2015=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2015\\2015ne.csv",
+                 sep=",", header=TRUE)
+ne2016=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2016\\2016ne.csv",
+                 sep=",", header=TRUE)
+ne2017=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2017\\2017ne.csv",
+                 sep=",", header=TRUE)
+ne2018=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2018\\2018ne.csv",
+                 sep=",", header=TRUE)
+ne2019=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2019\\2019ne.csv",
+                 sep=",", header=TRUE)
+
+
+#2019
+
+
+
+
+
+
+
+
+
+
