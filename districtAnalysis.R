751d0c01244e3106bbbf5116925797f227c94399
Mariah Bolden <ibotismariah@gmail.com>
2021-02-26 16:31:46 -0500

    districts are chartable

--- districtAnalysis.R
+++ districtAnalysis.R
@@ -14,7 +14,6 @@ fim2018=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-A
 fim2019=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2019\\2019fim.csv",
                    sep=",", header=TRUE)
 
-
 #Indiana 
 ind2015=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2015\\2015in.csv",
                    sep=",", header=TRUE)
@@ -34,7 +33,7 @@ fma2016=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-A
                    sep=",", header=TRUE)
 fma2017=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2017\\2017mar.csv",
                    sep=",", header=TRUE)
-fma2018=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2018\\2018fma.csv",
+fma2018=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2018\\2018mar.csv",
                    sep=",", header=TRUE)
 fma2019=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2019\\2019fma.csv",
                    sep=",", header=TRUE)
@@ -93,6 +92,119 @@ pch2019=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-A
 #texas
 tx2019=read.csv("C:\\Users\\iboti\\Documents\\Personal Projects\\FRC-District-Analysis\\districts2019\\2019tx.csv",
                  sep=",", header=TRUE)
+
+#lists of years in a district
+fim=list(fim2019,fim2018,fim2017,fim2016,fim2015) #Michigan
+ind=list(ind2019,ind2018,ind2017,ind2016,ind2015) #indiana
+fma=list(fma2019,fma2018,fma2017,fma2016,fma2015) #mid atlanticn
+ne=list(ne2019,ne2018,ne2017,ne2016,ne2015) #new england
+pnw=list(pnw2019,pnw2018,pnw2017,pnw2016,pnw2015) #pnw
+chs=list(chs2019, chs2018, chs2017,chs2016) #chesepeake
+nc=list(nc2019,nc2018,nc2017,nc2016) #NC
+pch=list(pch2019,pch2018,pch2017,pch2016) #peachtree 
+tx=list(tx2019) #texas
+
+
+#list of districts in year
+dist2015=list(fim2015,ind2015,fma2015, ne2015, pnw2015)
+dist2016=list(fim2016,ind2016,fma2016, ne2016, pnw2016, chs2016, nc2016, pch2016)
+dist2017=list(fim2017,ind2017,fma2017, ne2017, pnw2017, chs2017, nc2017, pch2017)
+dist2018=list(fim2018,ind2018,fma2018, ne2018, pnw2018, chs2018, nc2018, pch2018)
+dist2019=list(fim2019,ind2019,fma2019, ne2019, pnw2019, chs2019, nc2019, pch2019,tx2019)
+
+#colors for years in chartssss
+#2015:Green 2016:grey 2017: brown  2018: cyan  2019: darkblue
+colors=list('darkblue','cyan3','tan4','slategray','darkolivegreen')
+years=list('2019','2018','2017','2016','2015')
+
+
+
+#creates a function that graphs a district or set of years 
+rookieIncome = function(group){
+  i=1
+  for (year in group){
+    scatter.smooth(year$rookie_year, year$income,
+                   col=as.character(colors[i]),pch=19 ,
+                  ylab = "Income", xlab='Rookie Year')
+    fit = lm(year$rookie_year~year$income)
+    i=i+1
+  }
+}
+#creates a function that graphs a district or set of years 
+rookieTravel = function(group){
+  i=1
+  for (year in group){
+    scatter.smooth(year$rookie_year, year$average_distance,
+                   col=as.character(colors[i]),pch=19 ,
+                   ylab = "Distance Traveled For Events", xlab='Rookie Year')
+    fit = lm(year$rookie_year~year$income)
+    i=i+1
+  }
+}
+#creates a function that graphs a district or set of years 
+rookiePoints = function(group){
+  i=1
+  for (year in group){
+    scatter.smooth(year$rookie_year, year$points,
+                   col=as.character(colors[i]),pch=19 ,
+                   ylab = "District Points", xlab='Rookie Year')
+    fit = lm(year$rookie_year~year$income)
+    i=i+1
+  }
+}
+
+incomePoints = function(group){
+  i=1
+  for (year in group){
+    scatter.smooth(year$income, year$points,
+                   col=as.character(colors[i]),pch=19 ,
+                   ylab = "District Points", xlab='Median Income')
+    fit = lm(year$rookie_year~year$income)
+    i=i+1
+  }
+}
+
+incomeTravel = function(group){
+  i=1
+  for (year in group){
+    scatter.smooth(year$average_distance, year$income,
+                   col=as.character(colors[i]),pch=19 ,
+                   ylab = "income", xlab='Travel')
+    fit = lm(year$rookie_year~year$income)
+    i=i+1
+  }
+}
+
+TravelPoints = function(group){
+  i=1
+  for (year in group){
+    scatter.smooth(year$average_distance, year$points,
+                   col=as.character(colors[i]),pch=19 ,
+                   ylab = "District Points", xlab='distance')
+    fit = lm(year$rookie_year~year$income)
+    i=i+1
+  }
+}
+
+chartDist = function(group){
+  rookieIncome(group)
+  rookiePoints(group)
+  rookieTravel(group)
+  incomePoints(group)
+  incomeTravel(group)
+  TravelPoints(group)
+}
+
+#district by graph
+chartDist(fim)
+
+
+
+
+
+
+
+
 
 
 
